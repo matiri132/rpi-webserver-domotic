0c074b709acf051e98d5242dadc2593ff650910b
matiri132 <matiasmachado7@gmail.com>
2019-06-22 18:13:11 -0300

    arreglo3

--- lamp.c
+++ lamp.c
@@ -12,11 +12,8 @@ FILE *fp;
 char estado_actual[4];
 
 void status();
-void lamp();
 
 int main(int argc, char **argv){
-
-	
 
 	if(argv[1] != NULL){
 		fp = fopen ( "/proc/lampctrl", "rw" );        
@@ -27,37 +24,9 @@ int main(int argc, char **argv){
 		
 		if(strcmp(argv[1], "status") == 0){
 			status();
-		}else if(strcmp(argv[1], "lamp") == 0){
-			fgets(estado_actual, 4 , fp);
-			lamp();
-			}else{
-				printf("Valor de lampara invalido\n");
-			}
-			printf("Actualizado estado de las lamparas \n");
-			status();
-			fputs(estado_actual , fp);
-		}else{
-			printf("%s : No es un comando valido \n" , argv[1]);
-		}
-	}else{
-		printf("Debe ingresar parametros\n");
-	}
-
-	fclose ( fp );
-	return 0;
-    
-}
-
-void status(){
-	while (feof(fp) == 0){
- 				fgets(estado_actual,4,fp);
- 				printf("Estado actual:  (0: Apagado , 1: Encendido)\n");
- 				printf("L0: %c - L1: %c - L2: %c \n",estado_actual[0] , estado_actual[1] , estado_actual[2]);
- 	}
-}
-
-void lamp(){
-	if(strcmp(argv[2], "L0") == 0){
+			}else if(strcmp(argv[1], "lamp") == 0){
+				fgets(estado_actual, 4 , fp);
+				if(strcmp(argv[2], "L0") == 0){
 			     if(strcmp(argv[3], "0")== 0){
 			     	estado_actual[0] = '0';
 			     }else if(strcmp(argv[3], "1")== 0){
@@ -81,4 +50,26 @@ void lamp(){
 			     }else{
 			     	printf("Valor erroneo\n");
 			     }
+			}else{
+				printf("Valor de lampara invalido\n");
+			}
+			printf("Actualizado estado de las lamparas \n");
+			status();
+			fputs(estado_actual , fp);
+			}else{
+			printf("%s : No es un comando valido \n" , argv[1]);
+		}
+	}else{
+		printf("Debe ingresar parametros\n");
+	}
+	fclose ( fp );
+	return 0;
+}
+
+void status(){
+	while (feof(fp) == 0){
+ 				fgets(estado_actual,4,fp);
+ 				printf("Estado actual:  (0: Apagado , 1: Encendido)\n");
+ 				printf("L0: %c - L1: %c - L2: %c \n",estado_actual[0] , estado_actual[1] , estado_actual[2]);
+ 	}
 }
