4921e47d6f1af53b137f66024b7473d0cfa5311f
Machado <matiasmachado7@gmail.com>
2019-06-22 21:58:48 +0100

    arreglo

--- lamp.c
+++ lamp.c
@@ -10,46 +10,46 @@
 
 FILE *fp;
 char estado_actual[4];
-char nuevo_estado[4];
 
-int main(){
+int main(int argc, char **argv){
+
 	
-	if(argV[0] != NULL){
-		fp = fopen ( "/proc/lampctrl", "r+" );        
+
+	if(argv[1] != NULL){
+		fp = fopen ( "/proc/lampctrl", "rw" );        
 		if (fp==NULL) {
-			fputs ("File error",stderr);
+			fputs ("File error\n",stderr);
 			return 0;
 		}
-
-		if(strcmp(argV[0], "status")){
-			while (feof(archivo) == 0){
+		
+		if(strcmp(argv[1], "status") == 0){
+			while (feof(fp) == 0){
  				fgets(estado_actual,4,fp);
  				printf("Estado actual:  (0: Apagado , 1: Encendido)\n");
  				printf("L0: %c - L1: %c - L2: %c \n",estado_actual[0] , estado_actual[1] , estado_actual[2]);
- 	    	}
-		}
-
-		if(strcmp(argV[0], "lamp")){
-			if(strcmp(argV[1], "L0"){
-			     if(strcmp(argV[2], "0")){
+ 	    		}
+		}else if(strcmp(argv[1], "lamp") == 0){
+			fgets(estado_actual, 4 , fp);
+			if(strcmp(argv[2], "L0") == 0){
+			     if(strcmp(argv[3], "0")== 0){
 			     	estado_actual[0] = '0';
-			     }else if(strcmp(argV[2], "1"){
+			     }else if(strcmp(argv[3], "1")== 0){
 			     	estado_actual[0] = '1';
 			     }else{
 			     	printf("Valor erroneo\n");
 			     }
-			}else if(strcmp(argV[1], "L1"){
-			     if(strcmp(argV[2], "0")){
+			}else if(strcmp(argv[2], "L1")== 0){
+			     if(strcmp(argv[3], "0")==0){
 			     	estado_actual[1] = '0';
-			     }else if(strcmp(argV[2], "1"){
+			     }else if(strcmp(argv[3], "1")==0){
 			     	estado_actual[1] = '1';
 			     }else{
 			     	printf("Valor erroneo\n");
 			     }
-			}else if(strcmp(argV[1], "L2"){
-			     if(strcmp(argV[2], "0")){
+			}else if(strcmp(argv[2], "L2")==0){
+			     if(strcmp(argv[3], "0")==0){
 			     	estado_actual[2] = '0';
-			     }else if(strcmp(argV[2], "1"){
+			     }else if(strcmp(argv[3], "1")==0){
 			     	estado_actual[2] = '1';
 			     }else{
 			     	printf("Valor erroneo\n");
@@ -57,15 +57,13 @@ int main(){
 			}else{
 				printf("Valor de lampara invalido\n");
 			}
-
+			printf("Actualizado estado de las lamparas %s \n", estado_actual);
 			fputs(estado_actual , fp);
+		}else{
+			printf("%s : No es un comando valido \n" , argv[1]);
 		}
-
-
-    		
-	    
 	}else{
-		printf("No hay opciones validas\n");
+		printf("Debe ingresar parametros\n");
 	}
 
 	fclose ( fp );
