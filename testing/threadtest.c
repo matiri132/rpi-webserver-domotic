784b7743fb98af18f63b9216b1a7c4b05b1f1b40
matiri132 <matiasmachado7@gmail.com>
2019-06-26 15:17:55 -0300

    pwm

--- testing/threadtest.c
+++ testing/threadtest.c
@@ -31,7 +31,7 @@ module_param(tusec_Off, int, 0);
 
 int __init pwm_gpio_init(void)
 {
-	printk(KERN_INFO "PWM: starting %s. \n", __FUNCTION__);
+	printk("PWM: starting . \n", );
 	gpio_request(pwm1, "pwm1");
 	gpio_direction_output(pwm1, 0);
 	
@@ -40,17 +40,29 @@ int __init pwm_gpio_init(void)
 
 void __exit pwm_gpio_exit(void)
 {
-	printk(KERN_INFO "PWM: stopping %s.\n", __FUNCTION__);
+	printk("PWM: stopping \n");
 	gpio_free(pwm1);
 }
-/*--------------------------------------------------*/
-/*           Run PWM on the GPIO port               */
 
-int pwn_run_init(void)
-{
-	printk(KERN_ALERT "STARTING PWM: Frequency is %dMHz, and dutycycle is %d percent.\n", frequency, duty);
-	
-	/* Run PWM */
+/*--------------------------------------------------*/
+/*         Thread running calculations              */
+
+struct task_struct *task;
+struct task_struct *task2;	
+
+int pwm_thread(void *data){
+
+	while(1){
+	/* Calculate from frequency and dutycycle the delay-times */
+		tusec_On  = (1000000*duty)/(frequency*100);			// Duration of on-cycle
+		tusec_Off = (1000000*(100-duty))/(frequency*100);	// Duration of off-cycle
+		if (kthread_should_stop()) break;
+	}
+	return 0;
+}
+
+int pwm_thread2(void *data){
+
 	while(enable){	
 		gpio_set_value(pwm1, 1);
 		usleep_range(tusec_On, tusec_On);
@@ -60,44 +72,18 @@ int pwn_run_init(void)
 	return 0;
 }
 
-void pwm_run_exit(void)
-{
-	printk(KERN_ALERT "STOPPING PWM in %s.\n", __FUNCTION__);
-}
-
-/*--------------------------------------------------*/
-/*         Thread running calculations              */
-#define THREAD_NAME "pwm"
-
-struct task_struct *task;
-
-int pwm_thread(void *data)
-{
-
-	while(1){
-	/* Calculate from frequency and dutycycle the delay-times */
-		printk("Funcionando\n");
-		tusec_On  = (1000000*duty)/(frequency*100);			// Duration of on-cycle
-		tusec_Off = (1000000*(100-duty))/(frequency*100);	// Duration of off-cycle
-		printk("%d : %d " ,tusec_On , tusec_Off);
-		msleep(100);
-		gpio_set_value(pwm1 , status);
-		status = !status;
-		if (kthread_should_stop()) break;
-	}
-	return 0;
-}
-
 void pwm_thread_init(void)
 {
 	printk(KERN_INFO "PWM: started thread...");
-	task = kthread_run(pwm_thread, NULL, THREAD_NAME);
+	task = kthread_run(pwm_thread, NULL, "pwm1");
+	task2 = kthread_run(pwm_thread2, NULL , "pwm2")
 }
 
 void pwm_thread_exit(void)
 {
 	printk(KERN_INFO "PWM: stopping thread...");
 	kthread_stop(task);
+	kthread_stop(task2);
 }
 
 /*--------------------------------------------------*/
@@ -109,15 +95,13 @@ int __init pwm_init(void)
 	/* Configure initial state of the PWM pins */
 	pwm_gpio_init();
 	pwm_thread_init();
-	//pwn_run_init();
-
+	
 	return 0;
 }
 
 void __exit pwm_exit(void)  
 {
 	printk(KERN_INFO " Ending the function %s. \n", __FUNCTION__);
-	//pwm_run_exit();
 	pwm_thread_exit();
 	pwm_gpio_exit();
 	
