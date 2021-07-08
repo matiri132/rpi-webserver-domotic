a4dca43e8adbafe59996648fd775f4243bcee091
Mati Machado <matiasmachado@unc.edu.ar>
2021-07-08 12:33:02 -0300

    Delete .kernellamp.ko.cmd

--- kernelLamp/.kernellamp.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/pi/kmodules/kernelLamp/kernellamp.ko := ld -r  -EL -T ./scripts/module-common.lds -T ./arch/arm/kernel/module.lds  --build-id  -o /home/pi/kmodules/kernelLamp/kernellamp.ko /home/pi/kmodules/kernelLamp/kernellamp.o /home/pi/kmodules/kernelLamp/kernellamp.mod.o ;  true
