1192cc098fd10474dae9124868fe16e49275322e
Mati Machado <matiasmachado@unc.edu.ar>
2021-07-08 12:32:27 -0300

    Delete .kernelpwm.ko.cmd

--- kernelpwm/.kernelpwm.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/pi/kmodules/kernelpwm/kernelpwm.ko := ld -r  -EL -T ./scripts/module-common.lds -T ./arch/arm/kernel/module.lds  --build-id  -o /home/pi/kmodules/kernelpwm/kernelpwm.ko /home/pi/kmodules/kernelpwm/kernelpwm.o /home/pi/kmodules/kernelpwm/kernelpwm.mod.o ;  true
